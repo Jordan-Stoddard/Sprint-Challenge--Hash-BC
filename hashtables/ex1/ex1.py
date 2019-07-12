17c2364a3c6b39fccce9e4b7dae47bec4924a7e5
Jordan Stoddard <jjsincorporated@gmail.com>
2019-07-12 09:46:34 -0700

    Complete ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -6,18 +6,32 @@ from hashtables import (HashTable,
                         hash_table_resize)
 
 
+# loop through weights.
+# insert each weight into the hash as the key, and each weight index as the value
+# Check if limit - weight + current index = limit.
+# If it does, insert index of weight into set, if there's already an item in the set,
+# prepend or append depending on whether what's in the set is larger or smaller
+# than the new index.
+
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    indexSet = set()
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(len(weights)):
+        hash_table_insert(ht, weights[i], i)
 
-    return None
+    for i in range(len(weights)):
+        if hash_table_retrieve(ht, limit - weights[i]) is not None:
+                indexSet.add(i)
+    
+    if len(indexSet) == 0:
+        return None
+    else:
+        return sorted(indexSet, reverse=True)
 
 
 def print_answer(answer):
-    if answer is None:
-        print(str(answer[0] + " " + answer[1]))
+    if answer is not None:
+        print(f'{answer[0]} {answer[1]}')
     else:
         print("None")
